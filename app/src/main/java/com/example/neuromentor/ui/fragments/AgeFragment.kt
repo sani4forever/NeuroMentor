da3aab56d1168d6a5e37db654466b12be6592f16
sani4forever <sani4forever@gmail.com>
2025-06-22 14:16:12 +0300

    fixed skip

--- app/src/main/java/com/example/neuromentor/ui/fragments/AgeFragment.kt
+++ app/src/main/java/com/example/neuromentor/ui/fragments/AgeFragment.kt
@@ -48,8 +48,6 @@ class AgeFragment : Fragment() {
                 } else {
                     binding.ageEditText.error = getString(R.string.enter_your_age_0_99)
                 }
-            } else {
-                binding.ageEditText.error = getString(R.string.enter_your_age)
             }
         }
     }
