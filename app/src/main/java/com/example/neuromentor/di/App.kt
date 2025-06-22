61a56187782edd68f74cf9fb1218b97734482f37
Aleksandr <108301220+sani4forever@users.noreply.github.com>
2025-06-22 14:34:02 +0300

    Fixed DI, string resources in fragments, viewmodel save, fixed button size with different text (#7)
    * Fixed DI, string resources in fragments, viewmodel save, fixed button size with different text
    
    * fixed skip
    
    * fixed skip_btn, deleted age check in viewModel because it is already in fragment

---
+++ app/src/main/java/com/example/neuromentor/di/App.kt
@@ -0,0 +1,16 @@
+package com.example.neuromentor.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
--- app/src/main/java/com/example/neuromentor/viewmodels/PersonInfoViewModel.kt
+++ app/src/main/java/com/example/neuromentor/viewmodels/PersonInfoViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.neuromentor.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class PersonInfoViewModel : ViewModel() {
+    private val _age = MutableStateFlow<Int?>(null)
+    val age: StateFlow<Int?> = _age
+
+    private val _name = MutableStateFlow<String?>(null)
+    val name: StateFlow<String?> = _name
+
+    fun saveName(name: String?) {
+        _name.value = name
+    }
+
+    fun saveAge(age: Int) {
+        viewModelScope.launch {
+            _age.value = age
+        }
+    }
 
 }
